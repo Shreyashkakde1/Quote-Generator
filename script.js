99c0c7eabbab47e44cf1ce6255903f4553951b1f
Shreyash <shreyashkakde69@gmail.com>
2022-04-17 11:59:53 +0530

    checking if author is null or not and determining the size of quote text size by length and adjusting it's length

---
+++ script.js
@@ -0,0 +1,46 @@
+const quoteContainer = document.getElementById("quote-cotainer");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes Array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // console.log(quote);
+  // authorText.textContent = quote.author;
+
+  // Check if author field is blank and replace it with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote Length to determine styling
+  if (quote.text.length > 130) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+}
+
+// Get Qoutes From API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    // console.log(apiQuotes[12]);
+    newQuote();
+  } catch (error) {}
+}
+
+// On Load
+getQuotes();
+// newQuote();
